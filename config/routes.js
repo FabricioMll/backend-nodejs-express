bf0d97e76d03f6d27c2d2e7bfc8da26a6cf3182a
FABRICIO MELLO <97368075+FabricioMll@users.noreply.github.com>
2023-01-13 16:41:08 -0300

    Método Auth e Admin

--- config/routes.js
+++ config/routes.js
@@ -1,40 +1,47 @@
-// const category = require("../api/category")
+const admin = require('./admin')
 
 module.exports = app => {
     app.post('/signup', app.api.user.save)
     app.post('/signin', app.api.auth.signin)
     app.post('/validateToken', app.api.auth.validateToken)
 
-    app.get('/users', app.api.user.get)
+    app.route('/users')
+        .all(app.config.passport.authenticate())
+        .post(admin(app.api.user.save))
+        .get(admin(app.api.user.get))
     
     app.route('/users/:id')
-        .put(app.api.user.save)
+        .all(app.config.passport.authenticate())
+        .put(admin(app.api.user.save))
         .get(app.api.user.getById)
 
     app.route('/categories')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.get)
         .post(app.api.category.save)
 
     app.route('/categories/tree')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getTree)
 
     app.route('/categories/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getById)
-        .put(app.api.category.save)
-        .delete(app.api.category.remove)
+        .put(admin(app.api.category.save))
+        .delete(admin(app.api.category.remove))
 
     app.route('/categories/:id/articles')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.getByCategory)
 
     app.route('/articles')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.get)
-        .post(app.api.article.save)
+        .post(admin(app.api.article.save))
     
     app.route('/articles/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.getById)
-        .put(app.api.article.save)
-        .delete(app.api.article.remove)
-
-   
-
+        .put(admin(app.api.article.save))
+        .delete(admin(app.api.article.remove))
 }
